ec38ddca125499628e7742d4209d9fb04fefb533
nolnc <nolchua@gmail.com>
2024-11-07 12:50:17 -0800

    More small changes to text
    More small changes to text

--- src/App.jsx
+++ src/App.jsx
@@ -33,14 +33,15 @@ function App() {
   return (
     <div className="App">
       <h1>Multiple object detection using the MediaPipe Object Detector task</h1>
-      <div>This demo uses a model trained on the COCO dataset. It can identify 80 different classes of object in an image. <a href="https://github.com/amikelive/coco-labels/blob/master/coco-labels-2014_2017.txt" target="_blank" rel="noreferrer">See a list of available classes</a></div>
+      <div>This demo uses a model trained on the COCO dataset. It can identify 80 different classes of object in an image. <a href="https://github.com/amikelive/coco-labels/blob/master/coco-labels-2014_2017.txt" target="_blank" rel="noreferrer">See a list of available classes</a>
+        <p>Also, check out the repository for this project: <a href="https://github.com/nolnc/obj-detection-react" target="_blank" rel="noreferrer">obj-detection-react</a>.</p></div>
       <section id="demos" className="readyUnMask">
-        <h2>Demo: Detecting Images</h2>
+        <h2>Detecting Images</h2>
         <p><b>Upload</b> an image below then <b>click</b> on it to detect objects in the image.</p>
         <div className="detectOnClick">
           <ImageDropZone/>
         </div>
-        <h2>Demo: Webcam continuous detection</h2>
+        <h2>Webcam continuous detection</h2>
         <p>Hold some objects up close to your webcam to get a real-time detection! When ready click "enable webcam" below and accept access to the webcam.</p>
         <div id="liveView" className="videoView">
           <button id="webcamButton" className="mdc-button mdc-button--raised" onClick={handleWebcamButtonClick}>
